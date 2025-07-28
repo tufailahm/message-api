bbecd4b22f5acb5890ffd7c4c84ab3edb10ab82b
tufailahm <tufailahmedkhan@gmail.com>
2025-07-28 11:47:36 +0530

    tufail commits code

--- src/test/java/org/example/messageapi/MessageApiApplicationTests.java
+++ src/test/java/org/example/messageapi/MessageApiApplicationTests.java
@@ -2,12 +2,14 @@ package org.example.messageapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MessageApiApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(90,80);
     }
 
 }
